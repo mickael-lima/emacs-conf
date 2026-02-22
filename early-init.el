6597c8f8c89a888cd75b3246e550f7591e4f4097
Mickael Lima <smoek@tutanota.com>
2026-02-22 17:53:00 -0300

    `[up]` update `init.el`
    - add and configure the following packages:
    [auctex, cdlatex vertico, flycheck, corfu, orderless, magit]
    
    - other minor tweaks and classical hacking (i.e: emacs gc hack)
    - this commit enables the corfu auto-complete that can be anoying but
    I like it, emulates the vscode completion system.

--- early-init.el
+++ early-init.el
@@ -1,4 +1,16 @@
+
+;; Temporarily increase GC threshold during startup
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Restore to normal value after startup (e.g. 50MB)
+(add-hook 'emacs-startup-hook
+          (lambda () (setq gc-cons-threshold (* 50 1024 1024))))
+
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 
+(setq inhibit-startup-screen t)
+(setq native-comp-speed 2)
+
+(load-theme 'modus-vivendi-deuteranopia t)
--- init.el
+++ init.el
@@ -21,9 +21,126 @@
                (allow-no-window . t)))
 
 ;; Font
-(set-face-attribute 'default nil :font "Aporetic Sans Mono-12")
+(set-face-attribute 'default nil :font "AporeticSansMonoNerdFont-14")
 
-(use-package ef-themes
-  :ensure t
+;; Number line mode
+(global-display-line-numbers-mode)
+
+;; whick-key is now embeded in emacs
+(which-key-mode 1)
+
+;; Remember and restore the last cursor location of opened files
+(save-place-mode 1)
+
+;; Revert buffers when the underlying file has changed
+(global-auto-revert-mode 1)
+
+;; Why this is not the default
+(setq delete-selection-mode t)
+
+;; --- Packages ---
+
+;; AucTeX settings - from https://gist.github.com/karthink/7d89df35ee9b7ac0c93d0177b862dadb
+(use-package latex
+  :ensure auctex
+  :hook ((LaTeX-mode . prettify-symbols-mode))
+  :bind (:map LaTeX-mode-map
+         ("C-S-e" . latex-math-from-calc))
   :config
-  (load-theme 'ef-orange :no-confirm-loading))
+
+  ;; Format math as a Latex string with Calc
+  (defun latex-math-from-calc ()
+    "Evaluate `calc' on the contents of line at point."
+    (interactive)
+    (cond ((region-active-p)
+           (let* ((beg (region-beginning))
+                  (end (region-end))
+                  (string (buffer-substring-no-properties beg end)))
+             (kill-region beg end)
+             (insert (calc-eval `(,string calc-language latex
+                                          calc-prefer-frac t
+                                          calc-angle-mode rad)))))
+          (t (let ((l (thing-at-point 'line)))
+               (end-of-line 1) (kill-line 0)
+               (insert (calc-eval `(,l
+                                    calc-language latex
+                                    calc-prefer-frac t
+                                    calc-angle-mode rad))))))))
+
+(use-package preview
+  :after latex
+  :hook ((LaTeX-mode . preview-larger-previews))
+  :config
+  (defun preview-larger-previews ()
+    (setq preview-scale-function
+          (lambda () (* 1.25
+                   (funcall (preview-scale-from-face)))))))
+
+;; CDLatex settings
+(use-package cdlatex
+  :ensure t
+  :hook (LaTeX-mode . turn-on-cdlatex)
+  :bind (:map cdlatex-mode-map
+              ("<tab>" . cdlatex-tab)))
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+;; Enable Vertico.
+(use-package vertico
+  :ensure t
+  :custom
+  (vertico-scroll-margin 0) ;; Different scroll margin
+  (vertico-resize nil) ;; Grow and shrink the Vertico minibuffer
+
+  :init
+  (vertico-mode))
+
+(use-package corfu
+  :ensure t
+  
+  :custom
+  (corfu-cycle t)
+  (corfu-preselect 'prompt)
+
+  (corfu-auto t)
+  (corfu-auto-delay 0.2)
+  (corfu-auto-trigger ".") ;; Custom trigger characters
+  (corfu-quit-no-match 'separator) ;; or t
+  
+  :init
+  (global-corfu-mode))
+
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :ensure t
+  :init
+  (savehist-mode))
+
+;; Emacs minibuffer configurations.
+(use-package emacs
+  :custom
+  (context-menu-mode t)
+  (enable-recursive-minibuffers t)
+  (read-extended-command-predicate #'command-completion-default-include-p)
+  (tab-always-indent 'complete)
+  (text-mode-ispell-word-completion nil)
+  (read-extended-command-predicate #'command-completion-default-include-p)
+
+  ;; Do not allow the cursor in the minibuffer prompt
+  (minibuffer-prompt-properties
+   '(read-only t cursor-intangible t face minibuffer-prompt)))
+  
+;; orderless completion style for corfu
+(use-package orderless
+  :ensure t
+  :custom
+  ;; (orderless-style-dispatchers '(orderless-affix-dispatch))
+  ;; (orderless-component-separator #'orderless-escapable-split-on-space)
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles partial-completion))))
+  (completion-category-defaults nil)) ;; Disable defaults, use our settings)
+
+(use-package magit
+  :ensure t)
